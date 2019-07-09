54572ad7a2394f2f22dbb0114505a539fd63bb21
mainstringargs <mainstringargs@users.noreply.github.com>
2019-07-09 09:24:50 -0500

    Adding cap limiter

--- screeners/looseCANSLIMFullUSA.py
+++ screeners/looseCANSLIMFullUSA.py
@@ -15,7 +15,7 @@ def dump_to_csv(dirName):
     if not os.path.exists(dirName):
         os.makedirs(dirName)
 
-    filters = ['fa_epsqoq_o5','fa_salesqoq_o5','geo_usa','sh_instown_o10','ta_averagetruerange_o2.5','ta_sma20_pa','ta_sma200_pa','ta_sma50_pa']  # Shows companies in NASDAQ which are in the S&P500
+    filters = ['cap_midunder','fa_epsqoq_o5','fa_salesqoq_o5','geo_usa','sh_instown_o10','ta_averagetruerange_o2.5','ta_sma20_pa','ta_sma200_pa','ta_sma50_pa']  # Shows companies in NASDAQ which are in the S&P500
     stock_list = Screener(filters=filters, order='marketcap') 
 
 
